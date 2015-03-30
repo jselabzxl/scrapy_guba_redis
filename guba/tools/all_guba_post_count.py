22a4513211361380a6aa9ade9542fe7433e50da0
linhaobuaa <1257819385@qq.com>
2015-03-30 22:31:57 +0800

    linhao little fix

--- guba/tools/all_guba_post_count.py
+++ guba/tools/all_guba_post_count.py
@@ -2,6 +2,7 @@
 """统计所有股票的帖子总数
 """
 
+import time
 from config import _default_mongo, GUBA_POST_COLLECTION_PREFIX
 
 mongo = _default_mongo()
@@ -10,7 +11,11 @@ results = mongo.collection_names()
 collection_names = [r for r in results if r.startswith(GUBA_POST_COLLECTION_PREFIX)]
 
 total_count = 0
-for col in collection_names:
-    total_count += mongo[col].count()
+while 1:
+    start_count = total_count
+    total_count = 0
+    for col in collection_names:
+        total_count += mongo[col].count()
 
-print total_count
+    print total_count - start_count
+    time.sleep(60)
