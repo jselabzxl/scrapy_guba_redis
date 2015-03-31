8d6309995ee1291fdecc64deaa0dd9ad3434c851
taozhiqi <760782665@qq.com>
2015-03-31 15:07:20 +0800

    linhao little fix

--- guba/pipelines.py
+++ guba/pipelines.py
@@ -199,3 +199,40 @@ class MongodbPipeline(object):
 
         return item
 
+import connection
+from scrapy.utils.serialize import ScrapyJSONEncoder
+
+class RedisStoragePipeline(object):
+    def __init__(self, server):
+        self.server = server
+        self.encoder = ScrapyJSONEncoder()
+
+    @classmethod
+    def from_settings(cls, settings):
+        server = connection.from_settings(settings)
+        return cls(server)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls.from_settings(crawler.settings)
+
+    def process_item(self, item, spider):
+        return deferToThread(self._process_item, item, spider)
+
+    def _process_item(self, item, spider):
+        data = self.encoder.encode(item)
+        if isinstance(item, GubaPostListItem):
+            key = self.item_key_list(item, spider)
+        if isinstance(item, GubaPostDetailItem):
+            key = self.item_key_detail(item, spider)
+        self.server.rpush(key, data)
+
+        return item
+
+    def item_key_list(self, item, spider):
+        stock_id = item['stock_id']
+        return "%s:list_items" % stock_id
+
+    def item_key_detail(self, item, spider):
+        stock_id = item['stock_id']
+        return "%s:detail_items" % stock_id
