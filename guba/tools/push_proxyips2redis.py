c418b87bf1a350befabecd9c049a9d0f6f1b6df5
taozhiqi <760782665@qq.com>
2015-04-03 14:58:13 +0800

    linhao little fix

---
+++ guba/tools/push_proxyips2redis.py
@@ -0,0 +1,21 @@
+#-*- coding=utf-8 -*-
+
+import time
+from config import redis
+
+redis_key = "guba_proxy_ips:sorted_set"
+
+forbid_ips = set()
+with open('ip_forbid.txt') as f:
+    for line in f:
+        forbid_ips.add(line.strip())
+
+proxy_ips = set()
+with open('../proxy_ips.txt') as f:
+    for line in f:
+        proxy_ip = line.strip()
+        if proxy_ip not in forbid_ips:
+            proxy_ips.add(proxy_ip)
+
+for proxy_ip in proxy_ips:
+    redis.zincrby(redis_key, proxy_ip, amount=1)
