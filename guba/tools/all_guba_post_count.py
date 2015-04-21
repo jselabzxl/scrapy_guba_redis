1bc518d55bc7c6b9d5706a61ac8c06fdc76587b0
linhaobuaa <1257819385@qq.com>
2015-04-21 20:03:20 +0800

    linhao little fix

--- guba/tools/all_guba_post_count.py
+++ guba/tools/all_guba_post_count.py
@@ -7,8 +7,9 @@ import time
 from config import _default_mongo, GUBA_POST_COLLECTION_PREFIX
 
 mode = sys.argv[1] # list or detail
+host = sys.argv[2] # host: 219.224.135.94/219.224.135.95
 
-mongo = _default_mongo()
+mongo = _default_mongo(host=host)
 
 results = mongo.collection_names()
 collection_names = [r for r in results if r.startswith(GUBA_POST_COLLECTION_PREFIX)]
@@ -23,5 +24,5 @@ while 1:
         elif mode == 'list':
             total_count += mongo[col].find().count()
 
-    print mode, ': ', total_count - start_count
+    print mode, ': ', total_count - start_count, ' host:', host
     time.sleep(60)
