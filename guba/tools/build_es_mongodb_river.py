ffca62ba921dd1126aafcd913003d9a6d18645da
linhaobuaa <1257819385@qq.com>
2015-04-22 08:51:46 +0800

    linhao little fix

--- guba/tools/build_es_mongodb_river.py
+++ guba/tools/build_es_mongodb_river.py
@@ -2,23 +2,14 @@
 
 import os
 
-"""
-del_command_base = "curl -XDELETE 'http://%s:%s/_river/river_guba_post_%s'"
-with open("stock_ids_2742.txt") as f:
-    for line in f:
-        stock_id = line.strip()
-        command = del_command_base % ("219.224.135.94", "9200", stock_id)
-        output = os.popen(command)
-        print output.read()
-
-"""
 command_base = "curl -XPUT 'http://%s:%s/_river/river_guba_post_%s/_meta' -d '{ \"type\": \"mongodb\", \"mongodb\": { \"servers\": [ { \"host\": %s, \"port\": %s } ], \"db\": %s, \"collection\": %s}, \"index\": {\"name\": %s, \"type\": %s}}'"
 
 with open("stock_ids_2742.txt") as f:
     for line in f:
         stock_id = line.strip()
-        if stock_id == '600001':
-            continue
-        command = command_base % ("219.224.135.94", "9200", stock_id, "\"219.224.135.94\"", "\"27020\"", "\"guba\"", "\"post_stock_" + stock_id + "\"", "\"" + stock_id + "\"", "\"219_224_135_94_27020\"")
+        command = command_base % ("219.224.135.94", "9200", stock_id + "_95", "\"219.224.135.95\"", "\"27020\"", "\"guba\"", "\"post_stock_" + stock_id + "\"", "\"post_stock_" + stock_id + "\"", "\"219_224_135_95_27020\"")
+        output = os.popen(command)
+        print output.read()
+        command = command_base % ("219.224.135.94", "9200", stock_id + "_94", "\"219.224.135.94\"", "\"27020\"", "\"guba\"", "\"post_stock_" + stock_id + "\"", "\"post_stock_" + stock_id + "\"", "\"219_224_135_94_27020\"")
         output = os.popen(command)
         print output.read()
