4a80be99d37ee72c84b7957e84d7313194f4d0f0
linhaobuaa <1257819385@qq.com>
2015-04-22 11:42:24 +0800

    linhao little fix

--- guba/tools/build_es_mongodb_river.py
+++ guba/tools/build_es_mongodb_river.py
@@ -7,6 +7,8 @@ command_base = "curl -XPUT 'http://%s:%s/_river/river_guba_post_%s/_meta' -d '{ 
 with open("stock_ids_2742.txt") as f:
     for line in f:
         stock_id = line.strip()
+        if stock_id == '600001':
+            continue
         command = command_base % ("219.224.135.94", "9200", stock_id + "_95", "\"219.224.135.95\"", "\"27020\"", "\"guba\"", "\"post_stock_" + stock_id + "\"", "\"post_stock_" + stock_id + "\"", "\"219_224_135_95_27020\"")
         output = os.popen(command)
         print output.read()
