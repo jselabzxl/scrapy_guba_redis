7ad2d864b548e6a20806015b0e747ccab644ac4a
jselabzxl <15195817207@163.com>
2015-05-20 10:18:21 +0800

    Update build_es_mongodb_river.sh

--- guba/tools/build_es_mongodb_river.sh
+++ guba/tools/build_es_mongodb_river.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -XPUT 'http://219.224.135.94:9200/_river/test_river_guba_post/_meta' -d '{
+curl -XPUT 'http://172.17.13.207:9200/_river/test_river_guba_post/_meta' -d '{
   "type": "mongodb",
   "mongodb": { 
     "servers":
     [
-      { "host": "219.224.135.94", "port": "27020" }
+      { "host": "172.17.13.207", "port": "27020" }
     ],
     "db": "guba", 
     "collection": "post_stock_600010"
